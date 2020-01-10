e4ad339d65d0f205713ff03017c132a74ad86276
Roman Gryzunov <gryzunov@gmail.com>
2020-01-10 15:11:06 +0300

    Add project with unit tests.

---
+++ DataStructures.Tests/AvlTreeListTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class AvlTreeListTests
+    {
+        [Fact]
+        public void TestEmptyList()
+        {
+            var tree = new AvlTreeList<int, int>();
+            Assert.Empty(tree);
+            Assert.Null(tree.First);
+            Assert.Null(tree.Last);
+        }
+
+        [Fact]
+        public void TestListWithSingleItem()
+        {
+            var tree = new AvlTreeList<int, int>();
+            var added = tree.Add(1, 1);
+            Assert.True(added);
+            _ = Assert.Single(tree);
+            Assert.NotNull(tree.First);
+            Assert.NotNull(tree.Last);
+            Assert.Same(tree.First, tree.Last);
+        }
+    }
+}
