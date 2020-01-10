3c23bd51a92af7e9bd344dc454fc4a2053937441
Roman Gryzunov <gryzunov@gmail.com>
2020-01-10 21:40:23 +0300

    Add more tests for AvlTreeList.

--- DataStructures.Tests/AvlTreeListTests.cs
+++ DataStructures.Tests/AvlTreeListTests.cs
@@ -1,9 +1,36 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
 
 namespace DataStructures.Tests
 {
     public class AvlTreeListTests
     {
+        private const int ItemCount = 100;
+
+        private readonly int[] _values;
+        private readonly int[] _sortedValues;
+
+        public AvlTreeListTests()
+        {
+            var random = new Random();
+            var set = new HashSet<int>();
+            _values = new int[ItemCount];
+            _sortedValues = new int[ItemCount];
+            for (int i = 0; i < _values.Length; i++)
+            {
+                int number;
+                do
+                {
+                    number = random.Next(1000);
+                } while (!set.Add(number));
+                _values[i] = number;
+                _sortedValues[i] = number;
+            }
+            Array.Sort(_sortedValues);
+        }
+
         [Fact]
         public void TestEmptyList()
         {
@@ -28,21 +55,15 @@ namespace DataStructures.Tests
         [Fact]
         public void RandomInsertTest()
         {
-            var tree = new AvlTreeList<int, int>
-            {
-                { 15, 15 },
-                { 7, 7 },
-                { 6, 6 },
-                { 25, 25 },
-                { 20, 20 }
-            };
-            Assert.Equal(5, tree.Count);
-            Assert.Equal(6, tree.First.Key);
-            Assert.Equal(25, tree.Last.Key);
-            Assert.Equal(7, tree.First.Next.Key);
-            Assert.Equal(20, tree.Last.Prev.Key);
-            Assert.Equal(15, tree.First.Next.Next.Key);
-            Assert.Equal(15, tree.Last.Prev.Prev.Key);
+            var tree = new AvlTreeList<int, int>();
+            AddValues(tree);
+            Assert.Equal(ItemCount, tree.Count);
+            Assert.Equal(_sortedValues[0], tree.First.Key);
+            Assert.Equal(_sortedValues[ItemCount - 1], tree.Last.Key);
+            Assert.Equal(_sortedValues[1], tree.First.Next.Key);
+            Assert.Equal(_sortedValues[ItemCount - 2], tree.Last.Prev.Key);
+            Assert.Equal(_sortedValues[2], tree.First.Next.Next.Key);
+            Assert.Equal(_sortedValues[ItemCount - 3], tree.Last.Prev.Prev.Key);
         }
 
         [Fact]
@@ -57,6 +78,31 @@ namespace DataStructures.Tests
             Assert.Empty(tree);
             Assert.Null(tree.First);
             Assert.Null(tree.Last);
+        }
+
+        [Fact]
+        public void KeyCollectionEnumeratorTest()
+        {
+            var tree = new AvlTreeList<int, int>();
+            AddValues(tree);
+            Assert.True(_sortedValues.SequenceEqual(tree.Keys));
+        }
+
+        [Fact]
+        public void ValueCollectionEnumeratorTest()
+        {
+            var tree = new AvlTreeList<int, int>();
+            AddValues(tree);
+            Assert.True(_sortedValues.Select(i => i * 2).SequenceEqual(tree.Values));
+        }
+
+        private void AddValues(AvlTreeList<int, int> tree)
+        {
+            for (int i = 0; i < _values.Length; i++)
+            {
+                var number = _values[i];
+                tree.Add(number, number * 2);
+            }
         }
     }
 }
