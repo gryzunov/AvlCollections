8aa98000a199c32074633c1455a4fcf017252ccc
Roman Gryzunov <gryzunov@gmail.com>
2020-01-22 12:05:38 +0300

    Add balance and copy to array tests for AvlTree.

--- DataStructures.Tests/AvlTreeTests.cs
+++ DataStructures.Tests/AvlTreeTests.cs
@@ -1,9 +1,53 @@
 ﻿using System;
+using Xunit;
 
 namespace DataStructures.Tests
 {
     public class AvlTreeTests
     {
+        [Fact]
+        public void InsertBalanceTest()
+        {
+            var tree = new AvlTree<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                tree.Add(i);
+                CheckBalance(tree);
+            }
+        }
+
+        [Fact]
+        public void DeleteBalanceTest()
+        {
+            var tree = new AvlTree<int>();
+            for (int i = 1; i <= 100; i++)
+            {
+                tree.Add(i);
+            }
+            for (int i = 1; i <= 100; i++)
+            {
+                tree.Remove(i);
+                CheckBalance(tree);
+            }
+        }
+
+        [Fact]
+        public void CopyToArrayTest()
+        {
+            var tree = new AvlTree<int>();
+            var src = new int[100];
+            for (int i = 0; i < src.Length; i++)
+            {
+                src[i] = i + 1;
+                tree.Add(i + 1);
+            }
+
+            var dest = new int[100];
+            tree.CopyTo(dest, 0);
+
+            Assert.Equal(src, dest);
+        }
+
         private void CheckBalance(AvlTree<int> tree)
         {
             CheckDepth(tree.Root);
@@ -27,7 +71,7 @@ namespace DataStructures.Tests
                 }
                 else if (b == f - 1)
                 {
-                    if (node.Balance != 1)
+                    if (node.Balance != -1)
                     {
                         err = 1;
                     }
@@ -35,7 +79,7 @@ namespace DataStructures.Tests
                 }
                 else if (b - 1 == f)
                 {
-                    if (node.Balance != -1)
+                    if (node.Balance != 1)
                     {
                         err = 1;
                     }
