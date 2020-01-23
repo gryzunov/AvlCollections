3a8bfa35f113f4092fe55c9f81f3af0c5b73a7bf
Roman Gryzunov <gryzunov@gmail.com>
2020-01-23 15:23:42 +0300

    Add test for InOrderTreeWalk method of AvlTree.

--- DataStructures.Tests/AvlTreeTests.cs
+++ DataStructures.Tests/AvlTreeTests.cs
@@ -73,6 +73,28 @@ namespace DataStructures.Tests
             Assert.Equal(src, dest);
         }
 
+        [Fact]
+        public void InOrderTreeWalkTest()
+        {
+            var tree = new AvlTree<int>();
+            var src = new int[100];
+            for (int i = 0; i < src.Length; i++)
+            {
+                src[i] = i + 1;
+                tree.Add(i + 1);
+            }
+
+            var dest = new int[100];
+            var index = 0;
+            tree.InOrderTreeWalk(item =>
+            {
+                dest[index++] = item;
+                return true;
+            });
+
+            Assert.Equal(src, dest);
+        }
+
         private void CheckBalance(AvlTree<int> tree)
         {
             CheckDepth(tree.Root);
